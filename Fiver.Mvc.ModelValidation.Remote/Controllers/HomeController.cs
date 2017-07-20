98a8c16ba5f43a25b4849908e67368765d478780
Tahir Naushasd <t.naushad25@gmail.com>
2017-07-20 15:46:10 +0100

    [remote] attribute

---
+++ Fiver.Mvc.ModelValidation.Remote/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Fiver.Mvc.ModelValidation.Remote.Models.Home;
+using Newtonsoft.Json;
+
+namespace Fiver.Mvc.ModelValidation.Remote.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(EmployeeInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var json = JsonConvert.SerializeObject(model);
+            return Content(json);
+        }
+
+        public IActionResult ValidateEmployeeNo(string employeeNo)
+        {
+            if (employeeNo == "007")
+                return Json(data: "007 is already assigned to James Bond!");
+
+            return Json(data: true);
+        }
+    }
+}
---
+++ Fiver.Mvc.ModelValidation.Remote/Models/Home/EmployeeInputModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fiver.Mvc.ModelValidation.Remote.Models.Home
+{
+    public class EmployeeInputModel
+    {
+        [Required]
+        [Remote(action: "ValidateEmployeeNo", controller: "Home")]
+        public string EmployeeNo { get; set; }
+    }
+}
